0c570a840376ca584cd797268d0b46f12b8f81fc
sobek1985 <sobek1985@gmail.com>
2015-02-05 13:46:39 +0000

    Added extension methods

---
+++ Sitecore.Toolbox/Extensions/MediaItemExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.Resources.Media;
+
+namespace Sitecore.Toolbox.Extensions
+{
+    public static class MediaItemExtensions
+    {
+        public static string MediaUrl(this MediaItem mediaItem)
+        {
+            var mediaUrlOptions = new MediaUrlOptions() { };
+
+            return Sitecore.StringUtil.EnsurePrefix('/', MediaManager.GetMediaUrl(mediaItem, mediaUrlOptions));
+        }
+    }
+}
---
+++ Sitecore.Toolbox/Extensions/MultiListFieldExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using Sitecore.Data.Items;
+
+namespace Sitecore.Toolbox.Extensions
+{
+    public static class MultiListFieldExtensions
+    {
+        public static MediaItem[] GetMediaItems(this Sitecore.Data.Fields.MultilistField multilistField)
+        {
+            var items = multilistField.GetItems();
+
+            return items.Select(item => new MediaItem(item)).ToArray();
+        }
+
+    }
+}
