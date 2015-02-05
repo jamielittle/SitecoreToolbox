8cc40a75554bf1ab5ec723bd4e43b0e02f5a6251
jamie.little <jamie.little@calonwebsolutions.co.uk>
2015-02-05 13:31:42 +0000

    Initial Project Setup and addition of first class in Toolbox

---
+++ Sitecore.Toolbox/Sublayout/SublayoutBase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Data.Items;
+using Sitecore.Web.UI.WebControls;
+
+namespace Sitecore.Toolbox.Sublayout
+{
+    public class SublayoutBase : System.Web.UI.UserControl
+    {
+        public Item DataSourceItem
+        {
+            get
+            {
+                // Ensure at the very least Context Item is returned as a fallback
+                Item returnItem = Sitecore.Context.Item;
+
+                Sitecore.Web.UI.WebControls.Sublayout controlSublayout = (this.Parent) as Sitecore.Web.UI.WebControls.Sublayout;
+                if (controlSublayout != null)
+                {
+                    string dataSourceString = controlSublayout.DataSource;
+
+                    if (!string.IsNullOrEmpty(dataSourceString))
+                    {
+                        Item dbItem = Sitecore.Context.Database.GetItem(dataSourceString);
+
+                        if (dbItem != null)
+                        {
+                            returnItem = dbItem;
+                        }
+                    }
+                }
+
+                return returnItem;
+            }
+        }
+    }
+}
