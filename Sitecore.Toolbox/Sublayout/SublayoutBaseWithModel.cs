5f10db124c07d67ce5a546836f29fa7df48c0c47
jamie.little <jamie.little@calonwebsolutions.co.uk>
2015-03-17 06:08:49 +0000

    Added new variant of SubayoutBase to support Models

---
+++ Sitecore.Toolbox/Sublayout/SublayoutBaseWithModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Data.Items;
+using Sitecore.Web.UI.WebControls;
+using System.Collections.Specialized;
+using System.Web.UI;
+using Sitecore.Web;
+
+namespace Sitecore.Toolbox.Sublayout
+{
+    public abstract class SublayoutBase<T> : SublayoutBase
+    {
+        public T Model { get; set; }
+
+        public abstract void CreateModel();
+    }
+}
